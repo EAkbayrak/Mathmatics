aa29783901ea7735d5016030f5fe54ec91f62450
EAkbayrak <emreakbayrak94@gmail.com>
2022-09-27 23:18:29 +0100

    This will show all even numbers now

--- src/main/maths.java
+++ src/main/maths.java
@@ -3,7 +3,7 @@ package main;
 public class maths {
     public static void main(String[] args) {
 
-        for (int x = 1; x <=100 ; x+=2)
-            System.out.print(x + " ");
+        for (int x = 2; x <=100 ; x+=2)
+            System.out.print("This will show all even numbers" + x + " ");
     }
 }
